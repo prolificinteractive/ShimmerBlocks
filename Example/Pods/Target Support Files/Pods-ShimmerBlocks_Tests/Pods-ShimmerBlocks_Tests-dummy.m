95084ad7dc0d6fb8c7755b124d1dba4bb09fdf97
Jonathan Samudio <jonathan2457@gmail.com>
2018-08-07 11:29:06 -0400

    Updated travis build.

---
+++ Example/Pods/Target Support Files/Pods-ShimmerBlocks_Tests/Pods-ShimmerBlocks_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShimmerBlocks_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ShimmerBlocks_Tests
+@end
---
+++ Example/Pods/Target Support Files/ShimmerBlocks/ShimmerBlocks-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ShimmerBlocks : NSObject
+@end
+@implementation PodsDummy_ShimmerBlocks
+@end
