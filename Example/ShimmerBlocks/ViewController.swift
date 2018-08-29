7aa7f885137652f79533a9dcf4440bb56a0dcff0
Jonathan Samudio <jonathan2457@gmail.com>
2018-08-29 11:36:33 -0400

    Added logic to start loading state.

--- Example/ShimmerBlocks/ViewController.swift
+++ Example/ShimmerBlocks/ViewController.swift
@@ -13,11 +13,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
 
+    private var isLoading = false
+    private var networkTimer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.estimatedRowHeight = 130
         tableView.registerCellNib(cellClass: InfoTableViewCell.self)
+
         tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(refreshControlUpdated), for: .valueChanged)
+    }
+
+    @objc func refreshControlUpdated(_ sender: Any) {
+        guard let refreshControl = sender as? UIRefreshControl else {
+            return
+        }
+        isLoading = refreshControl.isRefreshing
+        tableView.reloadData()
+
+        if refreshControl.isRefreshing {
+            simulateNetworkCall()
+        }
+    }
+
+    private func simulateNetworkCall() {
+        networkTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: false, block: { [weak self] (_) in
+            if let refreshControl = self?.tableView.refreshControl {
+                refreshControl.endRefreshing()
+                self?.refreshControlUpdated(refreshControl)
+            }
+        })
     }
 
 }
@@ -30,13 +56,8 @@ extension ViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: InfoTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
-        cell.isLoading(true)
+        cell.isLoading(isLoading)
         return cell
     }
-
-}
-
-extension ViewController: UITableViewDelegate {
-
 
 }
